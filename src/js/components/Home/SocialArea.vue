3a15ddefdbd002566d7b0624454fab515b54a7ef
Dennis Nilsson <dennis@thedennis.se>
2020-05-10 20:12:01 +0200

    Uppdaterar social

--- src/js/components/Home/SocialArea.vue
+++ src/js/components/Home/SocialArea.vue
@@ -12,26 +12,34 @@
             </a>
           </div>
           <div class="px-5 h-5-rem">
+            <a v-bind:href="globalData[0].youtubeBonus">
             <p>
               <i class="fab fa-youtube text-danger fz-28 align-middle"></i> Bonuskanalen på Youtube
             </p>
+             </a>
           </div>
         </div>
         <div class="col-sm pt-4">
           <div class="px-5 h-4-rem">
+            <a v-bind:href="globalData[0].facebook">
             <p>
               <i class="fab fa-facebook-square text-primary fz-28 align-middle"></i> Kulpåhjul på Facebook
             </p>
+             </a>
           </div>
           <div class="px-5 h-4-rem">
+            <a v-bind:href="globalData[0].instagram">
             <p>
               <i class="fab fa-instagram-square text-info fz-28 align-middle"></i> Kulpåhjul på Instagram
             </p>
+             </a>
           </div>
           <div class="px-5 h-4-rem">
+            <a v-bind:href="globalData[0].snapchat">
             <p>
               <i class="fab fa-snapchat-square text-warning fz-28 align-middle"></i> Kulpåhjul på Snapchat
             </p>
+             </a>
           </div>
         </div>
       </div>
